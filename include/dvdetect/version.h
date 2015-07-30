a998b01698fdae1e6d94050ad54d4036b3770b9d
nschlia <nschlia@oblivion-software.de>
2015-07-30 22:44:11 +0200

    Update version to 0.70.0

--- include/dvdetect/version.h
+++ include/dvdetect/version.h
@@ -1,7 +1,7 @@
 /*
   dvdetect DVD detection & lookup library
 
-  Copyright (C) 2013 Norbert Schlia <nschlia@dvdetect.de>
+  Copyright (C) 2013-2015 Norbert Schlia <nschlia@dvdetect.de>
 
   This program is free software: you can redistribute it and/or modify
   it under the terms of the GNU LESSER GENERAL PUBLIC LICENSE as published by
@@ -17,27 +17,27 @@
   along with this program.  If not, see <http://www.gnu.org/licenses/>.
 */
 
-/** \file version.h 
+/** \file version.h
  *
  *  \brief A file containing the libdvdetect package version
  */
 
 //! LIBDVDETECT_VERSION can be used as a string in programs to show what version is used.
-#define LIBDVDETECT_VERSION             "0.60.0 x86_64-suse-linux-gnu"
+#define LIBDVDETECT_VERSION             "0.70.0 i686-pc-mingw32"
 
 //! LIBDVDETECT_PROTOCOL_VERSION is the current protocol version. Libraries supporting higher versions must always support all smaller versions as well.
 #define LIBDVDETECT_PROTOCOL_VERSION	0
 
-#define DVDETECT_FILEVERSION            0,60,0,0
-#define DVDETECT_FILEVERSION_STR        "0.60.0.0\0"
+#define DVDETECT_FILEVERSION            0,70,0,0
+#define DVDETECT_FILEVERSION_STR        "0.70.0.0\0"
 
-#define DVDETECT_PRODUCTVERSION         0,60,0,0
-#define DVDETECT_PRODUCTVERSION_STR     "0.60\0"
+#define DVDETECT_PRODUCTVERSION         0,70,0,0
+#define DVDETECT_PRODUCTVERSION_STR     "0.70\0"
 
 #define DVDETECT_COMPANYNAME_STR        "Oblivion Software/Norbert Schlia"
 #define DVDETECT_FILEDESCRIPTION_STR    "libdvdetect"
 #define DVDETECT_INTERNALNAME_STR       "libdvdetect"
-#define DVDETECT_LEGALCOPYRIGHT_STR     "Copyright (c) 2013 Oblivion Software/Norbert Schlia"
+#define DVDETECT_LEGALCOPYRIGHT_STR     "Copyright (c) 2013-2015 Oblivion Software/Norbert Schlia"
 #define DVDETECT_LEGALTRADEMARKS1_STR   "All Rights Reserved"
 #define DVDETECT_LEGALTRADEMARKS2_STR   DVDETECT_LEGALTRADEMARKS1_STR
 #define DVDETECT_ORIGINALFILENAME_STR   "dvdetect.dll"
